4d7b4614c888e2154a6baa9a30d94d66a687c93f
Aririi <37724510+Aririi@users.noreply.github.com>
2022-06-24 23:00:23 -0700

    modifying to seperate pack and script repos

--- script/main.py
+++ script/main.py
@@ -1,3 +1,13 @@
+import ctypes
 from git import Git
+from os import listdir, path, rename
+from sys import platform
+
+dirlist = [item for item in listdir("../") if path.isdir(path.join("../", item))]
+for folder in dirlist:
+    if folder == "git":
+        if platform == "win32":
+            ctypes.windll.kernel32.SetFileAttributesW("../git", 0x02)
+        rename("../git", "../.git")
 
 Git("../").pull("https://github.com/Sencres013/EmiTech.git --rebase")
