ecde4be65a3cfa973c5f540df81a2aa00b153b5b
Sencres013 <77643682+Sencres013@users.noreply.github.com>
2022-06-26 08:35:08 +0200

    Updated and fixed script

--- script/main.py
+++ script/main.py
@@ -1,61 +1,23 @@
-import git
-from git import Git, Repo, RemoteProgress
-import os.path
-from datetime import datetime
-pack_repo = 'https://github.com/Aririi/EmiTech.git'
-repo_path = "../"
-#repo clone progress function
+from git import Git
+from os import listdir, path
+from shutil import move, rmtree
 
-class CloneProgress(RemoteProgress):
-	def update(self, op_code, cur_count, max_count=None, message=''):
-		if message:
-			print(message)
+remoteRepoLink = "https://github.com/Aririi/EmiTech.git"
+localRepoPath = ".."
+localRepo = Git(localRepoPath)
 
-now = datetime.now()
-print("Running at", now)
+if path.exists(f"{localRepoPath}/.git") == False:
+    localRepo.clone(remoteRepoLink)
 
-#Clone repo on first time, determined if git folder exists
-git_exists = os.path.exists("../.git")
-if (git_exists != True):
-	print("First time run, downloading content")
-	#borrowed from https://stackoverflow.com/questions/2472552/python-way-to-clone-a-git-repository
+    # move everything from cloned repo to instance folder
+    for file in listdir(f"{localRepoPath}/EmiTech"):
+        move(path.join(f"{localRepoPath}/EmiTech", file), localRepoPath)
 
-	print('Cloning into %s' % pack_repo)
-	git.Repo.clone_from(pack_repo, repo_path, 
-       branch='master', progress=CloneProgress())
-	# class git_operation_clone():
-		# try:
-			# def __init__(self):
-				# self.DIR_NAME = repo_path
-				# self.REMOTE_URL = pack_repo
-
-			# def git_clone(self):
-				# if os.path.isdir(repo_path):
-					# shutil.rmtree(repo_path)
-				# os.mkdir(repo_path)
-				# repo = git.Repo.init(repo_path)
-				# origin = repo.create_remote('origin', pack_repo)
-				# origin.fetch()
-				# origin.pull(origin.refs[0].remote_head)
-		# except Exception as e:
-			# print(str(e))
-
+    # delete ./EmiTech
+    try:
+        rmtree("{localRepoPath}/EmiTech")
+    except OSError as err:
+        print(f"Couldn't delete cloned repository: {err.strerror}")
 else:
-#stashes any changes if a local file has been modified that is in the repo; ideally, the player will sort out this difference manually if they need to
-#however, only configs that need to be changed will be pushed, so thhis is unlikely to cause undesired behavior
-
-#making Repo object
-	#repo_path = os.getenv('GIT_REPO_PATH')
-	repo = Repo(repo_path)
-	if not repo.bare:
-		print('Repo at {} successfully loaded.'.format(repo_path))
-	else:
-		print('Could not load repository at {} :('.format(repo_path))
-	print(Git("../").status)
-	print("Stashing and pulling")
-	#git stash --include-untracked
-	#Git("../").stash(pack_repo --include-untracked")
-	repo.git.stash('save', "--include-untracked")
-	#git pull (fetch and merge), should not make any conflicts since changes were stashed
-	Git("../").pull(pack_repo)
-	print(Git("../").status())
+    localRepo.stash("--include-untracked")
+    localRepo.pull(remoteRepoLink)
